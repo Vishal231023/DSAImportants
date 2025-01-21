1bee8d5cd779adf5b443d2cb89f8f7680a6a2a1f
Vishal Saxena <v.saxena0509@gmail.com>
2025-01-22 02:32:28 +0530

    string advance started

---
+++ src/Strings/MediumProblems/ReverseEveryWordInString.java
@@ -0,0 +1,57 @@
+package Strings.MediumProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseEveryWordInString {
+    public static void main(String[] args) {
+
+        String s = "the sky is blue";
+        String ans = reverseWords(s);
+        System.out.println(ans);
+
+    }
+
+    public  static String reverseWords(String s) {
+        //Tc: O(n) + O(n)
+        //Sc: O(n)
+
+        List<String> list = new ArrayList<>();
+
+        int i =0;
+
+        int n = s.length();
+
+        while(i <n){
+
+            //task 1
+            while(i < n && s.charAt(i) == ' ') i++;
+
+            if(i>= n) break;
+            int start = i;
+
+            while(i< n && s.charAt(i) != ' ') i++;
+
+            int end = i-1;
+
+            String word = s.substring(start,end+1);
+            list.add(word);
+
+
+
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int j = list.size()-1;j>=0;j--){
+            sb.append(list.get(j));
+            if(j!=0){
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+
+    }
+}
