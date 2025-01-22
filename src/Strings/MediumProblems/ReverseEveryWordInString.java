e631ea053cad1efab83a000ac209a381c1111faa
Vishal Saxena <v.saxena0509@gmail.com>
2025-01-23 00:34:15 +0530

    updated code for reverse in the string

--- src/Strings/MediumProblems/ReverseEveryWordInString.java
+++ src/Strings/MediumProblems/ReverseEveryWordInString.java
@@ -6,10 +6,64 @@ import java.util.List;
 public class ReverseEveryWordInString {
     public static void main(String[] args) {
 
-        String s = "the sky is blue";
+        String s = " hello world ";
         String ans = reverseWordsBruteForce(s);
         System.out.println(ans);
 
+        String ans1 = reverseStringOptimalApproach(s);
+        System.out.println(ans1);
+
+
+    }
+
+    private static String reverseStringOptimalApproach(String s) {
+
+        char [] ch = s.toCharArray();
+        int n = ch.length;
+
+        reverseArray(ch,0,n-1);
+
+        int i =0;
+        int j=0;
+
+        int start = 0;
+        int end =0;
+
+        while(j<n){
+            while(j<n && ch[j] == ' ')j++;
+             start = i;
+
+             while( j< n && ch[j] != ' '){
+                 ch[i] = ch[j];
+                 i++;
+                 j++;
+             }
+             end = i-1;
+
+             reverseArray(ch,start,end);
+             if(i < n){
+                 ch[i++] = ' ';
+             }
+
+
+        }
+        while (i > 0 && ch[i - 1] == ' ') {
+            i--;
+        }
+
+        return new String(ch).substring(0,i);
+
+    }
+
+    private static void reverseArray(char[] ch, int start, int end) {
+
+        while(start < end){
+            char temp = ch[start];
+            ch[start] = ch[end];
+            ch[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     public  static String reverseWordsBruteForce(String s) {
